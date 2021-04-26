cf9cf710f9c9b0380a7088f0c49a818f3d39048c
Nick <nickstich@icloud.com>
2021-04-26 14:33:48 +0300

    update with ADO.NET assignment

---
+++ Assignments/II.ADO.NET/ADO.NETassign/ADO.NETassign/CRUDsql.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace ADO.NETassign
+{
+    public class CRUDsql
+    {
+        public void CreateTable(SqlConnection connection, string query)
+        {
+            using (var c = connection)
+            {
+                c.Open();
+                using (var cmd = new SqlCommand(query, c))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        public void InsertTable(SqlConnection connection, string query)
+        {
+            using (var c = connection)
+            {
+                using (var da = new SqlDataAdapter(query, c))
+                {
+
+                    try
+                    {
+                        c.Open();
+                        da.InsertCommand = new SqlCommand(query, c);
+                        da.InsertCommand.ExecuteNonQuery();
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+
+                }
+            }
+        }
+
+        public void SelectTable(SqlConnection connection, string query)
+        {
+            using (var c = connection)
+            {
+                using (DataAdapter da = new SqlDataAdapter(query, c))
+                {
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    DataTable dt = ds.Tables[0];
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        Console.WriteLine($"{dr["FirstName"]} : {dr["LastName"]}");
+                    }
+                }
+            }
+        }
+
+        public void Update(SqlConnection connection, string query)
+        {
+            using (var c = connection)
+            {
+                using var da = new SqlDataAdapter(query, c);
+                try
+                {
+                    c.Open();
+                    da.UpdateCommand = c.CreateCommand();
+                    da.UpdateCommand.CommandText = query;
+                    da.UpdateCommand.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        public void Delete(SqlConnection connection, string query)
+        {
+            using (var c = connection)
+            {
+                using var da = new SqlDataAdapter(query, c);
+                try
+                {
+                    c.Open();
+                    da.DeleteCommand = c.CreateCommand();
+                    da.DeleteCommand.CommandText = query;
+                    da.DeleteCommand.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Assignments/II.ADO.NET/ADO.NETassign/ADO.NETassign/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADO.NETassign
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var con = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=ADO.NETgo;Integrated Security=True");
+            string personQuery = @"CREATE TABLE person (           
+                                   id INT NOT NULL,
+                                   FirstName VARCHAR(45) NOT NULL,
+                                   LastName VARCHAR(45) NOT NULL,
+                                   car_id INT NULL,
+                                   PRIMARY KEY (id))";
+            string carQuery = @"CREATE TABLE car (           
+                                   id INT NOT NULL,
+                                   Model VARCHAR(45) NOT NULL,
+                                   TotalCost INT NOT NULL,
+                                   PRIMARY KEY (id))";
+
+            var crud = new CRUDsql();
+            //crud.CreateTable(con, personQuery);
+            //crud.CreateTable(con, carQuery);
+
+            var insertText = "INSERT INTO person(id,FirstName,LastName,car_id) VALUES (1,'Nicolae','Stici',1) ;";
+            var insertCar = "INSERT INTO car(id, Model, TotalCost) VALUES (1,'Ford Mustang',32500), (2,'Porche 911',64300),(3,'Tesla Model S',48000);";
+            //crud.InsertTable(con, insertText);
+            //crud.InsertTable(con,insertCar);
+
+            var selectText = "Select * from person";
+            //crud.SelectTable(con,selectText);
+
+            var updateCar = "UPDATE car set TotalCost = 100000 where Model like '%Porche%'";
+            //crud.Update(con, updateCar);
+
+            var deleteCar = "DELETE car where Model like '%Tesla%'";
+            crud.Delete(con, deleteCar);
+        }
+    }
+}
